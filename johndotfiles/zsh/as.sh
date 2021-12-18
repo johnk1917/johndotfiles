8a9b5d0c5167150ddf11bb4fe15de25c55963da6
johnk1917 <96326315+johnk1917@users.noreply.github.com>
2021-12-18 11:32:57 +0700

    Add files via upload

---
+++ johndotfiles/zsh/as.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
