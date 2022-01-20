b052c3fd5459f3a2c16ae027268a0e6cdbf214de
johnk1917 <96326315+johnk1917@users.noreply.github.com>
2022-01-20 17:54:49 +0700

    Add files via upload

---
+++ johndotfiles/cmus/lib.pl
@@ -0,0 +1,55 @@
+/home/john1917/Music/01 - Hellboy.mp3
+/home/john1917/Music/02 - Drive-By (feat Xavier Wulf).mp3
+/home/john1917/Music/03 - OMFG.mp3
+/home/john1917/Music/04 - The Song They Played (When I Crashed Into the Wall) [feat Lil Tracy].mp3
+/home/john1917/Music/05 - Fucked Up.mp3
+/home/john1917/Music/06 - Cobain (feat Lil Tracy).mp3
+/home/john1917/Music/07 - Gucci Mane.mp3
+/home/john1917/Music/08 - Interlude.mp3
+/home/john1917/Music/09 - Worlds Away.mp3
+/home/john1917/Music/10 - Red Drop Shawty (feat KirbLaGoop).mp3
+/home/john1917/Music/11 - Girls (feat Horsehead).mp3
+/home/john1917/Music/12 - Nose Ring.mp3
+/home/john1917/Music/13 - We Think Too Much.mp3
+/home/john1917/Music/14 - The Last Thing I Wanna Do.mp3
+/home/john1917/Music/15 - Walk Away As the Door Slams (feat Lil Tracy).mp3
+/home/john1917/Music/16 - Move On Be Strong.mp3
+/home/john1917/Music/01 - Broken Smile (My All).mp3
+/home/john1917/Music/02 - Runaway.mp3
+/home/john1917/Music/03 - Sex with My Ex.mp3
+/home/john1917/Music/04 - Cry Alone.mp3
+/home/john1917/Music/05 - Leanin.mp3
+/home/john1917/Music/06 - 16 Lines.mp3
+/home/john1917/Music/07 - Life is Beautiful.mp3
+/home/john1917/Music/08 - Hate Me.mp3
+/home/john1917/Music/09 - IDGAF.mp3
+/home/john1917/Music/10 - White Girl.mp3
+/home/john1917/Music/11 - Fingers.mp3
+/home/john1917/Music/12 - Falling Down (feat XXXTENTACION) (Bonus Track).mp3
+/home/john1917/Music/13 - Sunlight On Your Skin (feat ILoveMakonnen) (Bonus Track).mp3
+/home/john1917/Music/01 - Crybaby.mp3
+/home/john1917/Music/02 - Lil Jeep.mp3
+/home/john1917/Music/03 - Yesterday.mp3
+/home/john1917/Music/04 - Absolute In Doubt (feat Wicca Phase Springs Eternal).mp3
+/home/john1917/Music/05 - Ghost Girl.mp3
+/home/john1917/Music/06 - Big City Blues (feat Cold Hart).mp3
+/home/john1917/Music/07 - Skyscrapers [Love Now Cry Later].mp3
+/home/john1917/Music/08 - Falling 4 Me.mp3
+/home/john1917/Music/09 - Peep - Nineteen.mp3
+/home/john1917/Music/10 - White Tee (feat Yung Bruh).mp3
+/home/john1917/Music/11 - Driveway.mp3
+/home/john1917/Music/01 - Never Eat Never Sleep.mp3
+/home/john1917/Music/02 - WitchBlades.mp3
+/home/john1917/Music/03 - Dying Out West.mp3
+/home/john1917/Music/04 - Past the Castle Walls.mp3
+/home/john1917/Music/05 - Your Favorite Dress.mp3
+/home/john1917/Music/01 - Pain (feat Slug Christ).mp3
+/home/john1917/Music/02 - Castles.mp3
+/home/john1917/Music/03 - White Wine.mp3
+/home/john1917/Music/01 - Benz Truck (u0433u0435u043Bu0438u043A).mp3
+/home/john1917/Music/02 - Save That Shit.mp3
+/home/john1917/Music/03 - Awful Things (feat Lil Tracy).mp3
+/home/john1917/Music/04 - U Said.mp3
+/home/john1917/Music/05 - Better Off (Dying).mp3
+/home/john1917/Music/06 - The Brightside.mp3
+/home/john1917/Music/07 - Problems.mp3
